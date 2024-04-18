20653578464224437998fade03e53dce3953f72d
ngTamAn111103 <ng.t.ann2003@gmail.com>
2024-04-19 00:45:14 +0700

    Sửa lại className

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
-import { Login } from './Auth/Login.jsx'
-import { Register } from './Auth/Register.jsx'
+import { Login } from './pages/Login.jsx'
+import { Register } from './pages/Register.jsx'
 function App() {
 
 
