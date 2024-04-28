5556059f67ca1e0d8aa420ec1dbded61a5f9ff47
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-04-28 20:21:50 +0700

    chat, contacts

--- src/components_Index/Contacts/ContactList.jsx
+++ src/components_Index/Contacts/ContactList.jsx
@@ -1,16 +1,68 @@
-import GroupContact from "./GroupContact"
-function ContactList() { 
-    
-    return ( <>
+import { Header } from "../chat-leftsidebar/Header";
+import GroupContact from "./GroupContact";
+function ContactList() {
+  const nameList = [
+    {
+      A: [
+        { id: "1", name: "Albert Rodarte" },
+        { id: "2", name: "Allison Etter" },
+      ],
+    },
+    {
+      C: [{ id: "3", name: "Craig Smilley" }],
+    },
+    {
+      D: [
+        { id: "4", name: "Daniel Clay" },
+        { id: "5", name: "Doris Brown" },
+      ],
+    },
+    {
+      I: [{ id: "6", name: "Iris Wells" }],
+    },
+  ];
+  {
+    /* duyet mang */
+  }
+
+  return (
+    <>
+      <div className="overflow-auto">
         {/* Hiển thị Title và Input Search */}
+        <Header
+          title="Contacts"
+          isSearch={true}
+          placeholderSearch="Search users..."
+          extend={
+            <>
+              <div className="user-chat-nav float-end">
+                <div id="create-contact">
+                  <button
+                    data-modal-target="default-modal"
+                    data-modal-toggle="default-modal"
+                    className="focus:ring-blue-300 px-5 py-2.5 text-center text-sm font-medium text-[#7a7f9a] focus:outline-none"
+                    type="button"
+                  >
+                    <i className="fa-regular fa-user"></i>
+                  </button>
+                </div>
+              </div>
+            </>
+          }
+        ></Header>
 
         {/* Hiển thị danh sách liên hệ theo nhóm A-Z */}
-        <ul className="p-6">
-            <GroupContact letter="A" nameList={[{"name":"Alex"},{"name":"Alison"}]}></GroupContact>    
-            <GroupContact letter="B" nameList={[{"name":"Blex"},{"name":"Blison"}]}></GroupContact>    
-        </ul>    
-    
-    </>)
+        <div className="scrollbar-config mt-5  h-[calc(100vh_-_120px)] overflow-auto scroll-smooth p-6 focus:scroll-auto">
+          {nameList.map((element) => (
+            <GroupContact
+              letter={Object.keys(element)[0]}
+              nameList={element[Object.keys(element)[0]]}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ContactList
+export default ContactList;
--- src/components_Index/Contacts/GroupContact.jsx
+++ src/components_Index/Contacts/GroupContact.jsx
@@ -1,35 +1,40 @@
-function GroupContact ({
-    letter = "A",
-    nameList = [{"name": "ALex Alison"},{"name": "ALex Alison"}]
-}) { 
-    //Duyet mảng 
-    const listName = nameList.map((element, index)=> 
-    <li key={index} className="text-sm block min-w-[23.75rem] font-semibold px-5 py-2.5">
-        <div className="flex items-center">
-            <div className="name flex-1">
-            {element.name}
+/* 
+    Component GroupContact render 1 danh sách liên hệ theo bảng chữ cái 
+    @param1: letter: kí tự trong bảng chữ cái 
+    @param2: danh sách trong contact dạng json gồm id và name 
+        id dùng để cho nút hiện dropdown 
+        name hiện tên trong danh sách
+*/ 
 
-            </div>
-            <div className="dropdown">
-                ...
-            </div>
+import OptionContact from "./OptionContact";
+function GroupContact({
+  letter = "A",
+  nameList = [{id: "1", name: "ALex Alison" }, { id: "2", name: "ALex Alison" }],
+}) {
+  //Duyet mảng
+  const listName = nameList.map((element) => (
+    <li
+      key={element.id}
+      className="block max-w-[23.75rem] px-5 py-2.5 text-sm font-semibold"
+    >
+      <div className="flex items-center">
+        <div className="name flex-1 text-[#343A40]">{element.name}</div>
+        <div className="menu relative">
+          <OptionContact index = {element.id}></OptionContact>
         </div>
-    </li>)
-    return (
-        <>
-        {/* nếu là nhóm A : thì không có margin top */}
-        <div 
-               className = {(letter==="A")? "" : "mt-4"}      
-        >
-            <h4 className="p-4 text-[0.9375rem] text-primary font-bold">{letter}</h4>
-            <ul>
-                {listName}
-            </ul>
-            
-
-
-        </div>
-        </>
-    )
+      </div>
+    </li>
+  ));
+  return (
+    <>
+      {/* nếu là nhóm A : thì không có margin top */}
+      <div className={letter === "A" ? "" : "mt-4"}>
+        <h4 className="p-4 text-[0.9375rem] font-medium text-primary">
+          {letter}
+        </h4>
+        <ul>{listName}</ul>
+      </div>
+    </>
+  );
 }
-export default GroupContact 
+export default GroupContact;
