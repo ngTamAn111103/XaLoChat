048183992918c9819d19486cda19f7c39a91a72d
ngTamAn111103 <ng.t.ann2003@gmail.com>
2024-05-30 18:41:39 +0700

    Sử dụng zustand

--- src/App.jsx
+++ src/App.jsx
@@ -16,27 +16,28 @@ import Profile from "./components_Index/Profile.jsx";
 function App() {
   // TA: Backend
 
-    // lấy user
-    const user = false
-    useEffect(() =>{
-      const unSub = onAuthStateChanged(auth, (user) => {
-        console.log(user);
-      });
-      return () =>{
-        unSub();
-      };
-    },[]);
+  // theo dõi trạng thái xác thực của người dùng
+  const user = false; // Lưu trữ thông tin người dùng sau khi xác thực
+  useEffect(() => {
+    // onAuthStateChanged: một hàm lắng nghe từ Firebase Authentication
+    // Nó sẽ được gọi mỗi khi trạng thái xác thực của người dùng thay đổi (ví dụ: đăng nhập, đăng xuất, thay đổi thông tin người dùng).
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
   return (
     <>
       <Router>
         <Routes>
-        <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/index" element={<Index />} />
           <Route path="/forget-password" element={<RecoverPass />} />
           <Route path="/profile" element={<Profile />} />
-
         </Routes>
       </Router>
     </>
